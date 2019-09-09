8b88bb6f4ea310010bad8153c884158dd8776a37
Eaven <zyfheaven@163.com>
2019-09-09 17:37:36 +0800

    Initial commit

---
+++ src/main/java/com/pm/App.java
@@ -0,0 +1,14 @@
+package com.pm;
+
+/**
+ * Hello world!
+ * 根据该博客写的
+ *https://blog.csdn.net/wangmx1993328/article/details/83036285
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
